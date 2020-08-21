4cb2d7a9a2acbbc27b8c5aa1e54cc39065321d88
Eric Ferguson <ferguson.eric.r@gmail.com>
2020-08-21 15:50:17 -0700

    stack > completed + update to sll for length

--- stack/stack.py
+++ stack/stack.py
@@ -4,22 +4,35 @@ return elements in Last In First Out order.
 
 1. Implement the Stack class using an array as the underlying storage structure.
    Make sure the Stack tests pass.
+
 2. Re-implement the Stack class, this time using the linked list implementation
    as the underlying storage structure.
    Make sure the Stack tests pass.
+
 3. What is the difference between using an array vs. a linked list when 
    implementing a Stack?
+   
 """
+from singly_linked_list import LinkedList
+
 class Stack:
     def __init__(self):
         self.size = 0
-        # self.storage = ?
+        self.storage = LinkedList()
 
     def __len__(self):
-        pass
+        self.size = self.storage.length
+        return self.size
 
     def push(self, value):
-        pass
+        self.storage.add_to_tail(value)
+        self.size = self.storage.length
+        return value
 
     def pop(self):
-        pass
+        if self.size == 0:
+            return None
+        else:
+            removed = self.storage.tail.value
+            self.storage.remove_tail()
+            return removed
